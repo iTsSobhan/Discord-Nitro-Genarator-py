3983e5921767e1794852456f7476efd3cbfc0640
Sobhan <90289153+Sobhan-SRZA@users.noreply.github.com>
2021-11-07 07:17:23 +0330

    Add files via upload

---
+++ Nitrogen.py
@@ -0,0 +1,31 @@
+import discord
+import random
+import os
+import random
+import os
+from discord.ext import commands
+
+with open('helpcommand.txt', 'r') as f:
+    f_contents = f.read()
+message = (f_contents)
+def gen():
+    chars = ['a', 'b', 'c', 'd',  'e','f', 'g', 'h','i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+    '1','2','3','4','5','6','7','8','9','0'
+    ]
+    return "".join(random.choices(chars, k=16))
+
+link = "https://discord.gift/"
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == '.gen nitro':
+            await message.channel.send(link + gen())
+client = MyClient()
+client.run('TOKEN_HERE')
