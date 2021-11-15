09d680666f700d71730c481e9d447ef0abfa79a8
Sobhan <90289153+Sobhan-SRZA@users.noreply.github.com>
2021-11-15 12:43:57 +0330

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 from discord import DMChannel
 
 
-client = commands.Bot(command_prefix = ".") 
+client = commands.Bot(command_prefix = "place-prefix") 
 
 with open('helpcommand.txt', 'r') as f:
   f_contents = f.read()
@@ -17,13 +17,14 @@ embed=discord.Embed(title="Nitro Code Is Ready", url="https://discord.gg/xgcZNSj
 
 @client.command()
 async def gen(ctx):
+  if ctx.message.channel.id == (909003589479989258) or ctx.message.channel.id == (place-channel-id): #if you have some channel paste this code <or ctx.message.channel.id == (place-channel-id)> and enjoy :)
     user = await client.fetch_user(f"{ctx.author.id}")
     chars = ['a', 'b', 'c', 'd',  'e','f', 'g', 'h','i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
   '1','2','3','4','5','6','7','8','9','0'
   ]
     gen = "".join(random.choices(chars, k=16))
     await ctx.send(embed = embed)
-    await DMChannel.send(user,f"{link + gen}")    
+    await DMChannel.send(user,f"{link + gen}")       
 
 
 keep_alive()       
